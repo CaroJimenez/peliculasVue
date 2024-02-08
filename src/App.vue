d839d912e356c439af2c39aed9204057a44b4121
CaroJimenez <20213tn053@utez.edu.mx>
2024-02-07 18:59:36 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template class="main">
+  <div class="mainContainer">
+   
+  <h1>Películas</h1>
+  <div class="buttonContainer"><div>
+  <b-button variant="primary" v-b-modal.modal-1>Add movie here</b-button>
+</div></div>
+  <div class="moviesContainer">
+    
+    
+    <div class="cardContainer">
+  <b-card
+    title="Card Title"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      Some quick example text to build on the card title and make up the bulk of the card's content.
+    </b-card-text>
+
+    <b-button href="#" variant="primary">Go somewhere</b-button>
+  </b-card>
+</div>
+<div class="cardContainer"><h1>card</h1></div>
+
+  </div>
+<!--Modal-->
+<div>
+  <b-modal id="modal-1" title="BootstrapVue">
+    <p class="my-4">Hello from modal!</p>
+  </b-modal>
+</div>
+  </div>
+  
+</template>
+<script>
+export default{
+methods:{
+  addMovie(){
+    console.log("creando película...")
+  }
+
+}
+  
+}
+</script>
+
+<style >
+.main{
+  display: flex;
+}
+.mainContainer{
+  flex:1
+}
+.moviesContainer{
+  display: flex;
+}
+.cardContainer{
+  padding: 5%;
+}
+.buttonContainer{
+  display: flex;
+justify-content: end;
+padding: 1%;
+}
+</style>
---
+++ src/components/Loader.vue
@@ -0,0 +1,37 @@
+<template>
+    <form @submit.prevent="submit"
+          class="vl-parent"
+          ref="formContainer">
+        <!-- your form inputs goes here-->
+        <label><input type="checkbox" v-model="fullPage">Full page?</label>
+        <button type="submit">Login</button>
+    </form>
+</template>
+
+<script>
+import Loading from 'vue-loading-overlay';
+    import 'vue-loading-overlay/dist/css/index.css';
+    export default {
+        data() {
+            return {
+                isLoading: false,
+                fullPage: true
+            }
+        },
+        components: {
+            Loading
+        },
+        methods: {
+            doAjax() {
+                this.isLoading = true;
+                // simulate AJAX
+                setTimeout(() => {
+                    this.isLoading = false
+                }, 5000)
+            },
+            onCancel() {
+                console.log('User cancelled the loader.')
+            }
+        }
+    }
+</script>
